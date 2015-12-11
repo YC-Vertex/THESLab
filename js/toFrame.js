8c4516e72d0fd5d9fa2179c6145a4dd6bb7ec090
YC-Vertex <vertexprivate@gmail.com>
2015-12-11 23:20:11 +0800

    bug fixed

--- js/toFrame.js
+++ js/toFrame.js
@@ -1,9 +1,11 @@
-if (document.title == "简介 - THESLab") {
-  parent.window.frames["topFrame"].location = "about:blank";
-  parent.document.getElementById("frameSet").rows = "0,*"
-}
+window.onload = function() {
+  if (document.title == "简介 - THESLab") {
+    parent.window.frames["topFrame"].location = "about:blank";
+    parent.document.getElementById("frameSet").rows = "0,*"
+  }
 
-else {
-  parent.window.frames["topFrame"].location = "top.html";
-  parent.document.getElementById("frameSet").rows = "64,*"
+  else {
+    parent.window.frames["topFrame"].location = "top.html";
+    parent.document.getElementById("frameSet").rows = "64,*"
+  }
 }
