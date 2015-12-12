a9b71dafcea54f0bec81d5b6c577b7b2c3d4bba1
qhfz <vertexprivate@gmail.com>
2015-12-12 14:30:36 +0800

    bug fixed

--- js/toFrame.js
+++ js/toFrame.js
@@ -1,11 +1,3 @@
 window.onload = function() {
-  if (document.title == "简介 - THESLab") {
-    parent.window.frames["topFrame"].location = "about:blank";
-    parent.document.getElementById("frameSet").rows = "0,*"
-  }
-
-  else {
-    parent.window.frames["topFrame"].location = "top.html";
-    parent.document.getElementById("frameSet").rows = "64,*"
-  }
+	parent.document.getElementById("frameSet").rows = document.title == "简介 - THESLab" ? "0,*": "64,*";
 }
